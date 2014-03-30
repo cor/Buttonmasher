e7e188ba0773382c2a86d66c6e892f82cc699944
Cor Pruijs <cor@pruys.com>
2014-03-30 14:16:03 +0200

    Finsied building the codeDisplay

--- js/buttonmasher.js
+++ js/buttonmasher.js
@@ -30,20 +30,65 @@ $(document).ready(function(){
 		$(this).addClass("testButton_" + ((index % numberOfStyles) + 1));
 	});
 	
-
-
 	$(".testButton").click(function(event){	
 		
 		numberDisplayCount++;
 		$("div.numberDisplay").html(numberDisplayCount);
 		
 		//use jquery to check what button is pressed and convert it to 1-based
-		var buttonNumber = $(event.target).index(".testButton") + 1;
+		var lastEnteredButton = $(event.target).index(".testButton") + 1;
 		
+		//Change the colors in the colordisplay
+		if ($(".enteredCodeDisplay_1").css("background-color") == "rgb(255, 255, 255)" ) {
+
+				if (lastEnteredButton == 1) $(".enteredCodeDisplay_1").css("background-color", "rgb(235, 46, 34)");
+			 	else if (lastEnteredButton == 2) $(".enteredCodeDisplay_1").css("background-color", "rgb(235, 75, 61)");
+				else if (lastEnteredButton == 3) $(".enteredCodeDisplay_1").css("background-color", "rgb(235, 107, 64)");
+			 	else if (lastEnteredButton == 4) $(".enteredCodeDisplay_1").css("background-color", "rgb(226, 177, 49)");
+			 	else $(".enteredCodeDisplay_1").css("background-color", "pink");
+		}
+		else if ($(".enteredCodeDisplay_2").css("background-color") == "rgb(255, 255, 255)" ) {
+
+				if (lastEnteredButton == 1) $(".enteredCodeDisplay_2").css("background-color", "rgb(235, 46, 34)");
+			 	else if (lastEnteredButton == 2) $(".enteredCodeDisplay_2").css("background-color", "rgb(235, 75, 61)");
+				else if (lastEnteredButton == 3) $(".enteredCodeDisplay_2").css("background-color", "rgb(235, 107, 64)");
+			 	else if (lastEnteredButton == 4) $(".enteredCodeDisplay_2").css("background-color", "rgb(226, 177, 49)");
+			 	else $(".enteredCodeDisplay_2").css("background-color", "pink");
+		}
+		else if ($(".enteredCodeDisplay_3").css("background-color") == "rgb(255, 255, 255)" ) {
+
+				if (lastEnteredButton == 1) $(".enteredCodeDisplay_3").css("background-color", "rgb(235, 46, 34)");
+			 	else if (lastEnteredButton == 2) $(".enteredCodeDisplay_3").css("background-color", "rgb(235, 75, 61)");
+				else if (lastEnteredButton == 3) $(".enteredCodeDisplay_3").css("background-color", "rgb(235, 107, 64)");
+			 	else if (lastEnteredButton == 4) $(".enteredCodeDisplay_3").css("background-color", "rgb(226, 177, 49)");
+			 	else $(".enteredCodeDisplay_3").css("background-color", "pink");
+		}
+		else if ($(".enteredCodeDisplay_4").css("background-color") == "rgb(255, 255, 255)" ) {
+
+				if (lastEnteredButton == 1) $(".enteredCodeDisplay_4").css("background-color", "rgb(235, 46, 34)");
+			 	else if (lastEnteredButton == 2) $(".enteredCodeDisplay_4").css("background-color", "rgb(235, 75, 61)");
+				else if (lastEnteredButton == 3) $(".enteredCodeDisplay_4").css("background-color", "rgb(235, 107, 64)");
+			 	else if (lastEnteredButton == 4) $(".enteredCodeDisplay_4").css("background-color", "rgb(226, 177, 49)");
+			 	else $(".enteredCodeDisplay_4").css("background-color", "pink");
+		}
+		else {
+			$(".enteredCodeDisplay_1").css("background-color", "rgb(255, 255, 255)");
+			$(".enteredCodeDisplay_2").css("background-color", "rgb(255, 255, 255)");
+			$(".enteredCodeDisplay_3").css("background-color", "rgb(255, 255, 255)");
+			$(".enteredCodeDisplay_4").css("background-color", "rgb(255, 255, 255)");
+
+			if (lastEnteredButton == 1) $(".enteredCodeDisplay_1").css("background-color", "rgb(235, 46, 34)");
+			 	else if (lastEnteredButton == 2) $(".enteredCodeDisplay_1").css("background-color", "rgb(235, 75, 61)");
+				else if (lastEnteredButton == 3) $(".enteredCodeDisplay_1").css("background-color", "rgb(235, 107, 64)");
+			 	else if (lastEnteredButton == 4) $(".enteredCodeDisplay_1").css("background-color", "rgb(226, 177, 49)");
+			 	else $(".enteredCodeDisplay_1").css("background-color", "pink");
+		}
+
+
 		//Use a mathematical algorithm to store the pressed button in enteredCode
 		enteredCode = enteredCode % divider;
 		enteredCode *= numberSystem;
-		enteredCode += buttonNumber;
+		enteredCode += lastEnteredButton;
 
 		// convert entered code to a SHA-512 hash string 
 		var enteredCodeHashed = hex_sha512("" + enteredCode);
